c886d1067d2c3ff57ba18b2be1d636a54e04b602
leeoos <leonardo.colosi@gmail.com>
2023-05-09 10:59:20 +0200

    new start up command form matlab

--- start-matlab.sh
+++ start-matlab.sh
@@ -4,4 +4,4 @@ export ROB2LIB_PATH=$(pwd)"/lib"
 
 # starting matlab inside src dir
 # insert desired opening dir here
-setsid matlab -sd $(pwd) -desktop -c softwareopengl
+setsid matlab -sd $(pwd) -desktop -softwareopengl
